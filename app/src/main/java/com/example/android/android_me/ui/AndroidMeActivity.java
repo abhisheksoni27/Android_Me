129719530299515174a3ea3d65a06bab70c962cf
Abhishek Soni <abhisheksoni2720@gmail.com>
2018-08-16 16:47:52 +0530

    Dynamically set index of images: BodyPartFragment

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -16,9 +16,9 @@
 
 package com.example.android.android_me.ui;
 
+import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 import com.example.android.android_me.R;
 
