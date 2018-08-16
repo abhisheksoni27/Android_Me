435b90a935f629b6ad87c7b87c182d8f6d5e0af3
Abhishek Soni <abhisheksoni2720@gmail.com>
2018-08-16 17:40:33 +0530

    MasterListFragment done

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.android.android_me.ui;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
@@ -9,8 +8,8 @@ import com.example.android.android_me.R;
 
 public class MainActivity extends AppCompatActivity {
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 }
--- app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
@@ -1,8 +1,7 @@
 package com.example.android.android_me.ui;
 
+
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,26 +11,26 @@ import android.widget.GridView;
 import com.example.android.android_me.R;
 import com.example.android.android_me.data.AndroidImageAssets;
 
+// This fragment displays all of the AndroidMe images in one large list
+// The list appears as a grid of images
 public class MasterListFragment extends Fragment {
-
-    public MasterListAdapter masterListAdapter;
-    private GridView allImagesGrid;
-
+    // Mandatory empty constructor
     public MasterListFragment() {
-
     }
 
-    @Nullable
+    // Inflates the GridView of all AndroidMe images
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_master_list, container, false);
-
-        masterListAdapter = new MasterListAdapter(this.getContext(), AndroidImageAssets.getAll());
-
-        allImagesGrid = rootView.findViewById(R.id.all_images_grid);
-
-        allImagesGrid.setAdapter(masterListAdapter);
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.fragment_master_list, container, false);
+        // Get a reference to the GridView in the fragment_master_list xml layout file
+        GridView gridView = rootView.findViewById(R.id.all_images_grid);
+        // Create the adapter
+        // This adapter takes in the context and an ArrayList of ALL the image resources to display
+        MasterListAdapter mAdapter = new MasterListAdapter(getContext(), AndroidImageAssets.getAll());
+        // Set the adapter on the GridView
+        gridView.setAdapter(mAdapter);
+        // Return the root view
         return rootView;
     }
 }
